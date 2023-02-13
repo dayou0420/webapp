8a4fed61131dacc892310e545ed1b8eeabbd43ba
mo <dayoujianghong@gmail.com>
2023-02-14 08:10:11 +0900

    edit main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"flag"
-	"github.com/joho/godotenv"
 	"log"
 	"net/http"
-	"os"
 	"webapp/pkg/db"
 
 	"github.com/alexedwards/scs/v2"
@@ -22,20 +20,21 @@ func main() {
 	app := application{}
 
 	// github.com/joho/godotenv
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	env := os.Getenv("GO_ENV")
-	flag.StringVar(&app.DSN, "dsn", env, "Posgtres connection")
+	//err := godotenv.Load()
+	//if err != nil {
+	//	log.Fatal("Error loading .env file")
+	//}
+	//env := os.Getenv("GO_ENV")
+	//flag.StringVar(&app.DSN, "dsn", env, "Posgtres connection")
 
-	// flag.StringVar(&app.DSN, "dsn", "host=postgres port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5", "Posgtres connection")
+	flag.StringVar(&app.DSN, "dsn", "host=postgres port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5", "Posgtres connection")
 	flag.Parse()
 
 	conn, err := app.connectToDB()
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer conn.Close()
 
 	app.DB = db.PostgresConn{DB: conn}
 
