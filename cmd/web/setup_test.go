3a29363a42703cf93bfc5b1d46a4aa695ecb885f
mo <dayoujianghong@gmail.com>
2023-02-16 21:22:25 +0900

    edit handlers_test.go

--- cmd/web/setup_test.go
+++ cmd/web/setup_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 	"testing"
+	"webapp/pkg/db"
 )
 
 var app application
@@ -11,6 +14,20 @@ func TestMain(m *testing.M) {
 	pathToTemplates = "./../../templates/"
 
 	app.Session = getSession()
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	env := os.Getenv("GO_ENV")
+	app.DSN = env
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	app.DB = db.PostgresConn{DB: conn}
 
 	os.Exit(m.Run())
 }
